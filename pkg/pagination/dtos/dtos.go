46f1ec3518d0ca3003d038f3199c4bc43deaaa58
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2025-04-01 20:39:47 +0300

    update dtos validation

--- pkg/pagination/dtos/dtos.go
+++ pkg/pagination/dtos/dtos.go
@@ -1,11 +1,11 @@
 package dtos
 
-type PageQueryRequest struct {
-	Page *uint64 `form:"page" json:"page" binding:"omitempty,min=1"`
-	Size *uint64 `form:"size" json:"size" binding:"omitempty,min=1"`
+type PageParams struct {
+	Page *uint64 `form:"page" json:"page" validate:"omitempty,min=1"`
+	Size *uint64 `form:"size" json:"size" validate:"omitempty,min=1"`
 } // @name PaginationQueryDto
 
-type PageMetaResponse struct {
+type PageMeta struct {
 	Total uint64  `json:"total"`
 	Page  uint64  `json:"page"`
 	Size  uint64  `json:"size"`
