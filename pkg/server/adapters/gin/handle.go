0186da2ece9c88c573793b524465051a952d3995
Dmytro-Kucherenko <dmitro.kucherenko@gmail.com>
2025-04-12 23:23:41 +0300

    update server config

--- pkg/server/adapters/gin/handle.go
+++ pkg/server/adapters/gin/handle.go
@@ -2,98 +2,72 @@ package adapter
 
 import (
 	"net/http"
+	"reflect"
 
+	adapter "github.com/dmytro-kucherenko/smartner-utils-package/pkg/schema/adapters/playground"
 	"github.com/dmytro-kucherenko/smartner-utils-package/pkg/schema/modules/common"
 	"github.com/dmytro-kucherenko/smartner-utils-package/pkg/server"
 	"github.com/dmytro-kucherenko/smartner-utils-package/pkg/server/errors"
 	"github.com/gin-gonic/gin"
 )
 
-const DefaultTimeZone = "UTC"
-
 func handle[R any, P any](
 	request server.Request[R, P],
 	options *RequestConfig,
 	validateBody bool,
 ) []gin.HandlerFunc {
-	handlers := make([]gin.HandlerFunc, 0, len(options.Middlewares)+2)
-	for _, middleware := range options.Middlewares {
+	handlers := make([]gin.HandlerFunc, 0, len(options.Interceptors)+2)
+	for _, middleware := range options.Interceptors {
 		handlers = append(handlers, gin.HandlerFunc(middleware))
 	}
 
-	handlers = append(handlers, func(context *gin.Context) {
-		if options.ProvideSession && options.Meta.Session == nil {
-			context.Error(errors.NewHttpError(http.StatusUnauthorized, "User session was not found."))
-			context.Abort()
-
-			return
-		}
-
-		if options.Meta.Session == nil {
-			options.Meta.Session = &server.Session{}
-		}
-
+	handlers = append(handlers, func(c *gin.Context) {
 		abortValidationError := func(err error) {
-			context.Error(errors.NewHttpError(http.StatusBadRequest, "Validation Error", err.Error()))
-			context.Abort()
-		}
-
-		header, err := common.DecodeStruct[server.RequestHeader](context.Request.Header)
-		if err != nil {
-			abortValidationError(err)
-
-			return
-		}
-
-		timeZone := DefaultTimeZone
-		if len(header.TimeZone) > 0 {
-			timeZone = header.TimeZone[0]
+			c.Error(errors.NewHttpError(http.StatusBadRequest, "validation error", err.Error()))
+			c.Abort()
 		}
 
 		var params P
-		paramsSchema := common.ModifySchema(params)
-		if validateBody {
-			if err := context.ShouldBind(paramsSchema); err != nil {
+		if paramsType := reflect.TypeOf(params); paramsType != nil {
+			paramsSchema := common.ModifySchema(params)
+			if validateBody {
+				if err := c.ShouldBind(paramsSchema); err != nil {
+					abortValidationError(err)
+
+					return
+				}
+			}
+
+			if err := c.ShouldBindUri(paramsSchema); err != nil {
+				abortValidationError(err)
+
+				return
+			}
+
+			if err := c.ShouldBindQuery(paramsSchema); err != nil {
+				abortValidationError(err)
+
+				return
+			}
+
+			params = common.ParseSchema[P](paramsSchema)
+			if err := adapter.ValidateStruct(params); err != nil {
 				abortValidationError(err)
 
 				return
 			}
 		}
 
-		if err := context.ShouldBindUri(paramsSchema); err != nil {
-			abortValidationError(err)
-
-			return
-		}
-
-		if err := context.ShouldBindQuery(paramsSchema); err != nil {
-			abortValidationError(err)
-
-			return
-		}
-
-		params = common.ParseSchema[P](paramsSchema)
-		if err := options.Meta.Validator.Make(params); err != nil {
-			abortValidationError(err)
-
-			return
-		}
-
-		result, err := request(&server.RequestOptions[P]{
-			Params:   params,
-			Ctx:      context.Request.Context(),
-			Session:  *options.Meta.Session,
-			TimeZone: timeZone,
-		})
+		result, err := request(c.Request.Context(), params)
 
 		if err != nil {
-			context.Error(err)
-			context.Abort()
+			c.Error(err)
+			c.Abort()
 
 			return
 		}
 
-		context.JSON(options.Status, result)
+		c.JSON(options.Status, result)
 	})
 
 	return handlers
--- pkg/server/types.go
+++ pkg/server/types.go
@@ -2,40 +2,6 @@ package server
 
 import (
 	"context"
-	"net/http"
-	"time"
-
-	"github.com/dmytro-kucherenko/smartner-utils-package/pkg/types"
 )
 
-type RequestHeader struct {
-	TimeZone []string `json:"TimeZone" mapstructure:"timeZone" validate:"omitempty,max=1,dive,timezone"`
-}
-
-type RequestOptions[P any] struct {
-	Params   P
-	Ctx      context.Context
-	Session  Session
-	TimeZone string
-}
-
-type Request[R any, P any] func(data *RequestOptions[P]) (result R, err error)
-
-type Session struct {
-	UserID types.ID `json:"userId" mapstructure:"userId" validate:"required,uuid4"`
-}
-
-type ParamsValidator interface {
-	Make(data any) error
-}
-
-type RequestMeta struct {
-	Session   *Session
-	Validator ParamsValidator
-}
-
-type StartupOptions struct {
-	Server          *http.Server
-	ShutdownTimeout time.Duration
-	OnlyConfig      bool
-}
+type Request[R any, P any] func(ctx context.Context, params P) (result R, err error)
